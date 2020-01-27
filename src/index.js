d8a2ae9469ec470fcc727a283a29f20fd116fb7b
Muhammad Iqbal Muhyiddin <just.iqball@gmail.com>
2020-01-28 05:30:29 +0700

    refactor(project init): add bootstrap

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "containers/App";
 import * as serviceWorker from "./serviceWorker";
+import "styles/bootstrap.min.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
