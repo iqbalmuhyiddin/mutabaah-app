1d741ccecd75256c8d77d39f37db2e4e7a8fbfd9
Muhammad Iqbal Muhyiddin <just.iqball@gmail.com>
2020-01-28 09:38:40 +0700

    refactor(project init): import antd styles

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "containers/App";
 import * as serviceWorker from "./serviceWorker";
 import "styles/bootstrap.min.css";
+import "antd/dist/antd.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
